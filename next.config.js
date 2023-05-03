2e519511e9a2422af72b433831f46d1b9950b810
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-05-03 09:30:34 +0530

    created config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,56 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = () => {
+    /** @type {import('next').NextConfig} */
 
-module.exports = nextConfig
+    const nextConfig = {
+        distDir: 'build',
+        reactStrictMode: true,
+        env: {},
+        swcMinify: true,
+        pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+        publicRuntimeConfig: {
+            staticFolder: '/static',
+        },
+        images: {
+            deviceSizes: [350, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+            imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+            formats: ['image/avif', 'image/webp'],
+            minimumCacheTTL: 60,
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'images.unsplash.com',
+                },
+            ],
+        },
+        i18n: {
+            locales: ['en-US', 'en', 'de'],
+            defaultLocale: 'en-US',
+        },
+        webpack: (config, { isServer }) => {
+            const cfg = {
+                ...config,
+                resolve: {
+                    ...config.resolve,
+                },
+            };
+
+            if (!isServer) {
+                cfg.optimization.splitChunks = {
+                    chunks: 'all',
+                    minSize: 20480,
+                    cacheGroups: {
+                        default: {
+                            reuseExistingChunk: true,
+                            priority: -40,
+                            minChunks: 2,
+                        },
+                    },
+                };
+            }
+
+            return cfg;
+        },
+    };
+
+    return nextConfig;
+};
